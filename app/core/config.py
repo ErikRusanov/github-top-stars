8ae1ef3f3613ce8544d3c9d591b2b9135b5b82db
Erik Rusanov <erik.rusanov.03@gmail.com>
2024-03-03 11:29:02 +0300

    Docker settings added

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,10 @@
-from pydantic import PostgresDsn
+from pydantic import PostgresDsn, ConfigDict
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    model_config = ConfigDict(env_file=".env")
+
     # ------------- BASE --------------------------------------------
     BACKEND_CORS_ORIGINS: list[str] = ["*"]
 

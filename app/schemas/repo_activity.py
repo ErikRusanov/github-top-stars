45b6d2f7197a19f78e82c7469adff80aa2cc5dc7
Erik Rusanov <erik.rusanov.03@gmail.com>
2024-03-05 15:41:42 +0300

    Beautified code, added docs

--- app/schemas/repo_activity.py
+++ app/schemas/repo_activity.py
@@ -4,15 +4,27 @@ from pydantic import BaseModel
 
 
 class _RepoActivityBase(BaseModel):
+    """
+    Base Pydantic model for repository activity data.
+    """
+
     commits: int
     authors: list[str]
 
 
 class RepoActivity(_RepoActivityBase):
+    """
+    Pydantic model representing a repository activity with an additional 'date' field.
+    """
+
     date: date
 
 
 class RepoActivityCU(BaseModel):
+    """
+    Pydantic model for creating or updating repository activity data.
+    """
+
     commits: str
     authors: str
     date: str
--- app/schemas/repos.py
+++ app/schemas/repos.py
@@ -4,6 +4,10 @@ from pydantic import BaseModel
 
 
 class RepositorySort(Enum):
+    """
+    Enum representing sorting options for repositories.
+    """
+
     position_cur: str = "position_cur"
     position_prev: str = "position_prev"
     stars: str = "stars"
@@ -16,6 +20,10 @@ class RepositorySort(Enum):
 
 
 class _RepositoryBase(BaseModel):
+    """
+    Base Pydantic model for repository data.
+    """
+
     position_cur: int | None = None
     position_prev: int | None = None
     stars: int | None = None
@@ -26,12 +34,20 @@ class _RepositoryBase(BaseModel):
 
 
 class Repository(_RepositoryBase):
+    """
+    Pydantic model representing a repository with additional 'id', 'repo', and 'owner' fields.
+    """
+
     id: int | None = None
     repo: str
     owner: str
 
 
 class RepositoryCU(BaseModel):
+    """
+    Pydantic model for creating or updating repository data.
+    """
+
     position_cur: str
     position_prev: str
     stars: str

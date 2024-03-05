504dde2d23d54ff3f5df775c4f998877ca164c32
Erik Rusanov <erik.rusanov.03@gmail.com>
2024-03-05 15:11:18 +0300

    Changed interval to 1-minute

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -11,16 +11,16 @@ def configure_scheduler():
     This scheduler is designed to run the update_top_repos function at regular intervals, allowing
     for timely updates of top repositories based on star, view, fork, etc. counts.
 
-    The chosen frequency is set to every 5 minutes, providing a balance between quick updates and
+    The chosen frequency is set to every 1 minutes, providing a balance between quick updates and
     complying with authorized requests with a limit of 5000 requests per hour.
 
     :return: Configured AsyncIOScheduler instance.
     """
     scheduler = AsyncIOScheduler()
 
-    # Setting up cron jobs for each 5-minute interval within a 24-hour period
+    # Setting up cron jobs for each 1-minute interval within a 24-hour period
     for hour in range(0, 24):
-        for minute in range(0, 60, 5):
+        for minute in range(0, 60):
             trigger = CronTrigger(hour=hour, minute=minute)
             scheduler.add_job(
                 repos_service.update_top_repos,
